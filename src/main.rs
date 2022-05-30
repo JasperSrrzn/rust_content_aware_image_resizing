7438d63c8e6b988540dcb6cca9592205ec882d26
Jasper Sarrazin <Jasper.Sarrazin@tomtom.com>
2022-05-30 09:22:11 +0200

    adding structopt

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,60 @@
 use std::cmp::min;
 use std::collections::HashMap;
+use std::time::{Duration, Instant};
 
-use image::{GenericImage, Rgba};
+use image::{DynamicImage, GenericImage, GenericImageView, Pixel, Rgb, Rgba, RgbImage};
 use image_energy::energy_of_image::image_wrapper::ImageWrapper;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Options {
+    #[structopt(short = "i")]
+    inputpath: String,
+
+    #[structopt(short = "o")]
+    outputpath: String,
+}
 
 fn main() {
-    let image = ImageWrapper::from_file("./images/france.jpg");
-    let energy = image.get_energy();
-    let result = get_minimum_seam(&image, &energy);
-    let mut im = image.get_image().clone();
-    result.iter().for_each(|(x, y)| {
-        im.put_pixel(*x, *y, Rgba::from([255, 0, 0, 255]))
-    });
-    let image_result = im.save("./images/seam.jpg");
+    let options = Options::from_args();
+    let mut image = ImageWrapper::from_file(&options.inputpath);
+
+    println!("{}x{}", image.get_image().width(), image.get_image().height());
+
+    for n in 0..500 {
+        println!("Iteration {}", n);
+        image = crop(&image);
+        let image_result = image.get_image().save(format!("./images/video_france/output_{}.jpg", n));
+        match image_result {
+            Ok(_) => {}
+            Err(e) => panic!("{}", e)
+        }
+        println!("==========================");
+    }
+
+
+    println!("{}x{}", image.get_image().width(), image.get_image().height());
+    let image_result = image.get_image().save(&options.outputpath);
     match image_result {
         Ok(_) => {}
         Err(e) => panic!("{}", e)
     }
+}
+
+fn crop(image: &ImageWrapper) -> ImageWrapper {
+    let mut start = Instant::now();
+    let energy = image.get_energy();
+    let mut duration = start.elapsed();
+    println!("Energy calculation took {:?}", duration);
+    start = Instant::now();
+    let result = get_minimum_seam(&image, &energy);
+    duration = start.elapsed();
+    println!("Finding seam took {:?}", duration);
+    start = Instant::now();
+    let img = remove_seam_from_image(result, image.get_image());
+    duration = start.elapsed();
+    println!("Stripping took {:?}", duration);
+    ImageWrapper::new(img)
 }
 
 #[derive(Debug)]
@@ -99,12 +137,29 @@ fn get_minimum_seam(image: &ImageWrapper, energy: &HashMap<(u32, u32), u32>) -> 
         result.push(next.unwrap());
         next = seams.get(&next.unwrap()).unwrap().pointer;
     }
+    result.reverse();
     return result;
 }
 
 fn get_energy(x: u32, y: u32, energy: &HashMap<(u32, u32), u32>) -> u32 {
     return *energy.get(&(x, y)).unwrap();
 }
+
+fn remove_seam_from_image(seam: Vec<(u32, u32)>, image: &DynamicImage) -> DynamicImage {
+    let mut img = RgbImage::new(image.width() - 1, image.height());
+    image.pixels().for_each(|(x, y, c)| {
+        let pixel_to_remove: &(u32, u32) = seam.get(y as usize).unwrap();
+        if x < pixel_to_remove.0 {
+            img.put_pixel(x, y, c.to_rgb());
+        }
+
+        if x > pixel_to_remove.0 {
+            img.put_pixel(x - 1, y, c.to_rgb());
+        }
+    });
+    DynamicImage::from(img)
+}
+
 
 
 
